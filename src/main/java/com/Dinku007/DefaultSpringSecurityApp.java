abf67468b146699d28899731748dfaf2da13ebae
Anand <anamastemath@obsessory.com>
2020-10-22 20:05:15 +0530

    Initail commit

---
+++ src/main/java/com/Dinku007/DefaultSpringSecurityApp.java
@@ -0,0 +1,15 @@
+package com.Dinku007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Anand SN
+ */
+
+@SpringBootApplication
+public class DefaultSpringSecurityApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultSpringSecurityApp.class, args);
+    }
+}
---
+++ src/main/java/com/Dinku007/security/ApplicationSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.Dinku007.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Anand SN
+ */
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/home/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
